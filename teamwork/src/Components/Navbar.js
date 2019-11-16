78269dadcef889553fd1200436d24dd71e2ae984
Ozi Oviezibe <davovie@gmail.com>
2019-11-16 21:09:43 +0100

    updated

--- teamwork/src/Components/Navbar.js
+++ teamwork/src/Components/Navbar.js
@@ -3,12 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar">
+        <h1>Teamwork</h1>
         <ul>
             <li>News Update</li>
             <li>Help</li>
             <li>Log in</li>
         </ul>
-      <h1>This is my navbar</h1>
+      
     </nav>
   );
 };
