4abb8a8235cf001ade749bfb7585f0d1d47c1077
Ozi Oviezibe <davovie@gmail.com>
2019-11-17 18:46:14 +0100

    Updated Create form UI elements

--- teamwork/src/Components/CreateEmployeeForm.js
+++ teamwork/src/Components/CreateEmployeeForm.js
@@ -20,24 +20,6 @@ export default function CreateEmployee() {
       : setFormState({ ...formState, [name]: value });
   };
 
-  //   async postData(url = '', data = {}) {
-  //     // Default options are marked with *
-  //     const response = await fetch(url, {
-  //       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  //       mode: 'cors', // no-cors, *cors, same-origin
-  //       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  //       credentials: 'same-origin', // include, *same-origin, omit
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //         // 'Content-Type': 'application/x-www-form-urlencoded',
-  //       },
-  //       redirect: 'follow', // manual, *follow, error
-  //       referrer: 'no-referrer', // no-referrer, *client
-  //       body: JSON.stringify(data) // body data type must match "Content-Type" header
-  //     });
-  //     return await response.json(); // parses JSON response into native JavaScript objects
-  //   }
-
   const handleSubmit = async event => {
     event.preventDefault();
     const url = "http://localhost:3500/api/v1/auth/create-user";
@@ -45,9 +27,7 @@ export default function CreateEmployee() {
     try {
       const response = await fetch(url, {
         method: "POST", // or 'PUT'
-        body: JSON.stringify(
-          formState
-        ), // data can be `string` or {object}!
+        body: JSON.stringify(formState), // data can be `string` or {object}!
         headers: {
           "Content-Type": "application/json"
           //   'Content-Type': 'application/x-www-form-urlencoded'
@@ -58,103 +38,121 @@ export default function CreateEmployee() {
     } catch (error) {
       console.error("Error:", error);
     }
-
-    // try {
-    //     const data = await postData('localhost:3500/api/v1/auth/create-user', formState);
-    //     console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
   };
 
   return (
-    <div>
-      <p>Create Teamwork Employee Account:</p>
+    <div className="form_create_account">
+      <p className="center">Create Teamwork Employee Account:</p>
       <form onSubmit={handleSubmit}>
-        <label>First Name: </label>
-        <label>Last Name:</label>
-        <br />
-        <input
-          type="text"
-          name="firstName"
-          value={formState.firstName}
-          placeholder="First Name"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={formState.lastName}
-          placeholder="Last Name"
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label>email</label>
-        <input
-          type="text"
-          name="email"
-          placeholder="email"
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          name="confirmpassword"
-          placeholder="Confirm Password"
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label>Gender</label>
-        <label>
-          Male
+        <label className="form_label col-one-half">
+          <span className="label_text">First Name</span>
           <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={formState.gender === "male"}
+            type="text"
+            name="firstName"
+            value={formState.firstName}
+            placeholder="First Name"
             onChange={handleChange}
             required
           />
         </label>
-        <label>
-          Female
+        <label className="form_label col-one-half">
+          <span className="label_text">Last Name</span>
           <input
-            type="radio"
-            name="gender"
-            value="female"
-            checked={formState.gender === "female"}
+            type="text"
+            name="lastName"
+            value={formState.lastName}
+            placeholder="Last Name"
             onChange={handleChange}
             required
           />
         </label>
-        <br />
-        <label>Job Role</label>
-        <input
-          type="text"
-          name="jobRole"
-          placeholder="Job Role"
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label>
-          Department:
+        <label className="form_label">
+          <span className="label_text">email</span>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label className="password col-one-half">
+          <span className="label_text">Password</span>
+          <button
+            className="toggle-visibility"
+            title="toggle password visibility"
+            tabindex="-1"
+          >
+            <span className="glyphicon glyphicon-eye-close"></span>
+          </button>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label className="password col-one-half">
+          <span className="label_text">Confirm Password</span>
+          <button
+            className="toggle-visibility"
+            title="toggle password visibility"
+            tabindex="-1"
+          >
+            <span className="glyphicon glyphicon-eye-close"></span>
+          </button>
+          <input
+            type="password"
+            name="confirmpassword"
+            placeholder="Retype Password"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label className="gender col-one-half">
+          <span className="radio_text label_text">Gender</span>
+          <label className="radiobtn">
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={formState.gender === "male"}
+              onChange={handleChange}
+              required
+            />Male
+          </label>
+          <label className="radiobtn">
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={formState.gender === "female"}
+              onChange={handleChange}
+              required
+            />Female
+          </label>
+        </label>
+        <label className="form_label col-one-half">
+          <span className="label_text">Job Role</span>
+          <input
+            type="text"
+            name="jobRole"
+            placeholder="Job Role"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <div>
+
+        <label className="select_label" for="department">
+          <span className="select_text">Department</span>
+          </label>
+
           <select
+          className="select"
             name="department"
+            id="department"
             value={formState.department}
             onChange={handleChange}
             required
@@ -169,21 +167,22 @@ export default function CreateEmployee() {
             <option value="Human Resources">Human Resources</option>
             <option value="Design Group">Design Group</option>
           </select>
-        </label>
-        <br />
+        </div>
+
         <textarea
+          className="form_label"
           name="address"
           value={formState.address}
           placeholder="Enter address"
           onChange={handleChange}
-          rows="10"
-          cols="49"
+          rows="5"
+          cols="50"
           required
         />
-        <h1>
-          {formState.firstName} {formState.lastName} {formState.jobrole}
-        </h1>
-        <button type="submit">Submit</button>
+        <br />
+        <button type="submit" className="submit">
+          Create Account
+        </button>
       </form>
     </div>
   );
--- teamwork/src/Components/Navbar.js
+++ teamwork/src/Components/Navbar.js
@@ -3,11 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <h1>Teamwork</h1>
-        <ul>
-            <li>News Update</li>
-            <li>Help</li>
-            <li>Log in</li>
+        <h1 className="navbar_heading">Teamwork</h1>
+        <ul className="navbar_list">
+            <li className="navbar_group"><a href="#" className="navbar_link">News Update</a></li>
+            <li className="navbar_group"><a href="#" className="navbar_link">Help</a></li>
+            <li className="navbar_group"><a href="#" className="navbar_link">Log in</a></li>
         </ul>
       
     </nav>
